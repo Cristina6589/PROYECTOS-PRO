a3434df78c356886afc9635fdd55c08ecaed94df
Cristina6589 <cristinaromesanchez@gmail.com>
2026-06-15 18:26:24 +0200

    primer código y //TODO

--- TEMA1/ProyectoInicial/src/Main.java
+++ TEMA1/ProyectoInicial/src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Este es mi primer programa en Java");
+        System.out.println("Esta es la segunda línea del programa en Java\n");
+        /* La \n se usa como salto de línea, en lugar de usar println puedes usar
+        print y \n detrás de cualquiera de las palabras */
+    }
+
+    //TODO clase del 02/10/2025 (para ver esto VIEW - TOOL WINDOWS - TODO
+
 }
