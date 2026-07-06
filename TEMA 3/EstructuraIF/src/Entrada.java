56213540fc26418fda44c4eeb5061661d6992554
Cristina6589 <cristinaromesanchez@gmail.com>
2026-07-06 18:36:58 +0200

    IFELSEIF

--- TEMA 3/EstructuraIF/src/Entrada.java
+++ TEMA 3/EstructuraIF/src/Entrada.java
@@ -4,7 +4,6 @@ public class Entrada {
     public static void main(String[] args) {
         Decisiones variableDecisiones = new Decisiones();
         variableDecisiones.estructuraIF(8);
-
-
+        variableDecisiones.estructuraIFELSEIF(7.50);
     }
 }
--- TEMA 3/EstructuraIF/src/model/Decisiones.java
+++ TEMA 3/EstructuraIF/src/model/Decisiones.java
@@ -10,4 +10,36 @@ public class Decisiones {
         }
         System.out.println("Terminando la evaluación");
     }
+
+    public void estructuraIFELSEIF(double nota){
+        // 0 -> desastroso
+        // 1 - 3.99 -> mal
+        // 4 - 4.99 -> casi
+        // 5 - 7.99 -> bien
+        // 8 - 8.99 -> notable
+        // 9 - 9.99 -> sobresaliente
+        // 10 -> máquina
+        System.out.println("Iniciando la evaluación del examen");
+        if (nota>=0 && nota<=10){
+            if (nota<1){
+                System.out.println("Examen desastroso");
+            } else if (nota<4) {
+                System.out.println("Examen mal");
+            }else if (nota<5){
+                System.out.println("Examen casi aprobado");
+            }else if (nota<8){
+                System.out.println("Examen bien");
+            }else if (nota<9){
+                System.out.println("Examen notable");
+            }else if (nota<10){
+                System.out.println("Examen sobresaliente");
+            }else {
+                System.out.println("¡Eres una máquina!");
+            }
+        } else {
+            System.out.println("Rango incorrecto");
+        }
+
+        System.out.println("Evaluación finalizada");
+    }
 }
